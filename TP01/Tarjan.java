667b215751d83688cb47f1829ab1b1055edac303
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-09-30 15:46:56 -0300

    add tarjan branch and class

---
+++ TP01/Tarjan.java
@@ -0,0 +1,12 @@
+public class Tarjan {
+    // Atributes
+    private AdjacentList graph;
+
+    // Constuctor
+    public Tarjan(AdjacentList graph) {
+        this.graph = graph;
+    }
+
+    // Methods
+    
+}
