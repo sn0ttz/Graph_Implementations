7d855f9648648dbc54544a66894989acef863c6f
sn0ttz <dpmaia0503@gmail.com>
2024-10-28 13:08:08 -0300

    imp3 kinda working?

--- Implementacao_03/Graph.java
+++ Implementacao_03/Graph.java
@@ -1,57 +1,96 @@
 package Implementacao_03;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 //this implementation was heavily inspired by the one on the following link: https://www.programiz.com/dsa/graph-adjacency-list
 public class Graph {
-    private LinkedList<Vertex> adjListArray[];
+    LinkedList<Edge> adjListArray[];
+    int numVertices;
 
+    @SuppressWarnings("unchecked")
     Graph(int numVertices) {
-        numVertices++;
+        this.numVertices = numVertices;
         adjListArray = new LinkedList[numVertices];
-        for (int i = 1; i < numVertices; i++) {
+        for (int i = 0; i < numVertices; i++) {
             adjListArray[i] = new LinkedList<>();
         }
     }
 
-    public void addSucessor(int vertex, int sucessor, int flux) {
-        adjListArray[vertex].add(new Vertex(sucessor, flux));
+    public void addEdge(int source, int destiny, int capacity) {
+        Edge directEdge = new Edge(destiny, capacity);
+        Edge reverseEdge = new Edge(source, 0);
+
+        directEdge.reverse = reverseEdge;
+        reverseEdge.reverse = directEdge;
+
+        adjListArray[source].add(directEdge);
+        adjListArray[destiny].add(reverseEdge);
     }
 
-    public void getSucessors(int vertex) {
-        for (Vertex pCrawl : adjListArray[vertex]) {
-            System.out.print(" -> " + pCrawl.number + "(" + pCrawl.flux + ")");
-        }
-    }
+    public boolean BFS(int source, int destiny, int[] parent, Edge[] parentEdges) {
+        boolean[] visited = new boolean[adjListArray.length];
+        Arrays.fill(visited, false);
 
-    public void getPredecessors(int vertex) {
-        for (int i = 1; i < adjListArray.length; i++) {
-            for (Vertex pCrawl : adjListArray[i]) {
-                if (pCrawl.number == vertex) {
-                    System.out.print(" -> " + i + "(" + pCrawl.flux + ")");
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        visited[source] = true;
+        parent[source] = -1;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+
+            for (Edge edge : adjListArray[u]) {
+                int v = edge.destiny;
+
+                if (!visited[v] && edge.residualCap() > 0) {
+                    if (v == destiny) {
+                        parent[v] = u;
+                        parentEdges[v] = edge;
+                        return true;
+                    }
+                    queue.add(v);
+                    parent[v] = u;
+                    parentEdges[v] = edge;
+                    visited[v] = true;
                 }
             }
         }
-    }
 
-    public void printGraph() {
-        for (int v = 1; v < adjListArray.length; v++) {
-            System.out.println("Adjacency list of vertex " + v);
-            System.out.print("head");
-            for (Vertex pCrawl : adjListArray[v]) {
-                System.out.print(" -> " + pCrawl.number + "(" + pCrawl.flux + ")");
-            }
-            System.out.println("\n");
-        }
+        return false;
     }
 }
 
-class Vertex {
-    int number;
+class Edge {
+    int source;
+    int destiny;
     int flux;
+    int maxFlux;
+    Edge reverse;
 
-    Vertex(int n, int f) {
-        this.number = n;
-        this.flux = f;
+    Edge(int s, int d, int max) {
+        this.source = s;
+        this.destiny = d;
+        this.flux = 0;
+        this.maxFlux = max;
+    }
+
+    // inicializing the flux as zero here, so i dont have to do it on the
+    // FordFulkerson algorithm
+    Edge(int d, int max) {
+        this.destiny = d;
+        this.flux = 0;
+        this.maxFlux = max;
+        this.reverse = null;
+    }
+
+    public void addFlux(int flux) {
+        this.flux += flux;
+        this.reverse.flux -= flux;
+    }
+
+    public int residualCap() {
+        return maxFlux - flux;
     }
 }
--- Implementacao_03/Main.java
+++ Implementacao_03/Main.java
@@ -1,14 +1,74 @@
 package Implementacao_03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import Implementacao_03.FordFulkerson.Result;
+
 public class Main {
-    public static void main(String[] args) {
-        Graph graph = new Graph(5);
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("output.txt");
+        BufferedReader reader = new BufferedReader(new FileReader("Implementacao_03/RandomTrees/randomTree10.txt"));
+        writer.write("Random Tree time:" + "\n");
+        for (int i = 10; i < 100000; i *= 10) {
+            Long start = System.nanoTime();
 
-        graph.addSucessor(1, 2, 1);
-        graph.addSucessor(1, 3, 1);
-        graph.addSucessor(2, 4, 1);
-        graph.addSucessor(3, 4, 1);
+            reader = new BufferedReader(new FileReader("Implementacao_03/RandomTrees/randomTree" + i + ".txt"));
 
-        graph.printGraph();
+            String line = reader.readLine();
+            int[] values = treatString(line);
+
+            Graph graph = new Graph(values[0]);
+
+            while ((line = reader.readLine()) != null) {
+                values = treatString(line);
+                graph.addEdge(values[0], values[1], values[2]);
+            }
+
+            writer.write("Nodes: " + i + '\n');
+
+            Result result = FordFulkerson.fordFulkerson(graph, 0, graph.numVertices - 1);
+
+            writer.write("Max Flow: " + result.maxFlow + '\n');
+
+            long end = System.nanoTime();
+            writer.write("Time: " + (end - start) / 1000000 + "ms\n");
+        }
+        writer.write("------------------------------------------------------" + "\n");
+        writer.write("Random Graph time:" + "\n");
+
+        for (int i = 10; i < 100000; i *= 10) {
+            long start = System.nanoTime();
+            reader = new BufferedReader(new FileReader("Implementacao_03/RandomGraphs/randomGraph" + i + ".txt"));
+            String line = reader.readLine();
+            int[] values = treatString(line);
+            Graph graph = new Graph(values[0]);
+            while ((line = reader.readLine()) != null) {
+                values = treatString(line);
+                graph.addEdge(values[0], values[1], values[2]);
+            }
+
+            writer.write("Nodes: " + i + '\n');
+            Result result = FordFulkerson.fordFulkerson(graph, 0, graph.numVertices - 1);
+            writer.write("Max Flow: " + result.maxFlow + '\n');
+            long end = System.nanoTime();
+            writer.write("Time: " + (end - start) / 1000000 + "ms\n");
+        }
+
+        reader.close();
+        writer.close();
+    }
+
+    public static int[] treatString(String line) {
+        String[] values = line.split(" ");
+        int[] intValues = new int[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+            intValues[i] = Integer.parseInt(values[i]);
+        }
+
+        return intValues;
     }
 }
